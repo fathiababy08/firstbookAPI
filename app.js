e68d3e2b4baad544c7dcf5c8cf3cfa9e091bc096
fathiababy08 <fathiaodusola@gmail.com>
2023-04-13 14:28:24 +0100

    refactored port for deployment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 2100
+const PORT = process.env.PORT || 2100
 const mongoose = require('mongoose')
 const bookRouters = require('./routers/books')
 const customerRouters = require('./routers/customer')
